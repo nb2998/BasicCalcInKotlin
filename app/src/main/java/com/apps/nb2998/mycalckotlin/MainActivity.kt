207dbdec1a59af0b803e6003ba787d469b9326b1
Nishtha Bhatia <nbhatia329@gmail.com>
2018-08-21 18:49:14 +0530

    Anko Card View

--- app/src/main/java/com/apps/nb2998/mycalckotlin/MainActivity.kt
+++ app/src/main/java/com/apps/nb2998/mycalckotlin/MainActivity.kt
@@ -3,9 +3,15 @@ package com.apps.nb2998.mycalckotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.view.Gravity
+import android.widget.TextView
 import org.jetbrains.anko.*
+import org.jetbrains.anko.cardview.v7.cardView
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tvRes: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         var num2 = 0
 
         verticalLayout {
-            var et1 = editText {
+            val et1 = editText {
                 id = R.id.et1
                 hint = "Enter 1st number"
                 width = wrapContent
@@ -22,7 +28,7 @@ class MainActivity : AppCompatActivity() {
                 inputType = InputType.TYPE_CLASS_NUMBER
             }
 
-            var et2 = editText {
+            val et2 = editText {
                 id = R.id.et2
                 hint = "Enter 1st number"
                 width = wrapContent
@@ -30,56 +36,84 @@ class MainActivity : AppCompatActivity() {
                 inputType = InputType.TYPE_CLASS_NUMBER
             }
 
-            var tvRes = textView {
-                id = R.id.tvRes
-                text = ""
+            cardView {
+                verticalLayout {
+                    padding = dip(10)
+                    tvRes = textView {
+                        id = R.id.tvRes
+                        text = ""
+                        textSize = 36F
+                        gravity = Gravity.CENTER
+
+                    }.lparams {
+                        gravity = Gravity.CENTER
+                        width = matchParent
+                        height = wrapContent
+                    }
+                }
+            }.lparams{
+                margin = dip(20)
+                width= matchParent
+                height = wrapContent
             }
 
-            linearLayout {
-                button {
-                    id = R.id.btn_add
-                    text = "+"
-                    width = wrapContent
-                    height = wrapContent
-                    onClick {
-                        num1 = if (et1.text.isNotEmpty()) et1.text.toString().toInt() else 0
-                        num2 = if (et2.text.isNotEmpty()) et2.text.toString().toInt() else 0
-                        tvRes.text = (num1 + num2).toString()
+            verticalLayout {
+                linearLayout {
+                    button {
+                        id = R.id.btn_add
+                        text = "+"
+                        onClick {
+                            num1 = if (et1.text.isNotEmpty()) et1.text.toString().toInt() else 0
+                            num2 = if (et2.text.isNotEmpty()) et2.text.toString().toInt() else 0
+                            tvRes.text = (num1 + num2).toString()
+                        }
+                    }.lparams {
+                        width = dip(0)
+                        height = wrapContent
+                        weight = 1F
                     }
-                }
 
-                button {
-                    id = R.id.btn_sub
-                    text = "-"
-                    width = wrapContent
-                    height = wrapContent
-                    onClick {
-                        num1 = if (et1.text.isNotEmpty()) et1.text.toString().toInt() else 0
-                        num2 = if (et2.text.isNotEmpty()) et2.text.toString().toInt() else 0
-                        tvRes.text = (num1 - num2).toString()
+                    button {
+                        id = R.id.btn_sub
+                        text = "-"
+                        onClick {
+                            num1 = if (et1.text.isNotEmpty()) et1.text.toString().toInt() else 0
+                            num2 = if (et2.text.isNotEmpty()) et2.text.toString().toInt() else 0
+                            tvRes.text = (num1 - num2).toString()
+                        }
+                    }.lparams {
+                        width = dip(0)
+                        height = wrapContent
+                        weight = 1F
                     }
-                }
 
-                button {
-                    id = R.id.btn_mult
-                    text = "*"
-                    width = wrapContent
-                    height = wrapContent
-                    onClick {
-                        num1 = if (et1.text.isNotEmpty()) et1.text.toString().toInt() else 0
-                        num2 = if (et2.text.isNotEmpty()) et2.text.toString().toInt() else 0
-                        tvRes.text = (num1 * num2).toString()
-                    }
                 }
-                button {
-                    id = R.id.btn_divide
-                    text = "/"
-                    width = wrapContent
-                    height = wrapContent
-                    onClick {
-                        num1 = if (et1.text.isNotEmpty()) et1.text.toString().toInt() else 0
-                        num2 = if (et2.text.isNotEmpty()) et2.text.toString().toInt() else 0
-                        tvRes.text = if (num2 != 0) (num1 / num2).toString() else "Not possible"
+                linearLayout {
+                    button {
+                        id = R.id.btn_mult
+                        text = "*"
+                        onClick {
+                            num1 = if (et1.text.isNotEmpty()) et1.text.toString().toInt() else 0
+                            num2 = if (et2.text.isNotEmpty()) et2.text.toString().toInt() else 0
+                            tvRes.text = (num1 * num2).toString()
+                        }
+                    }.lparams {
+                        width = dip(0)
+                        height = wrapContent
+                        weight = 1F
+                    }
+                    button {
+                        id = R.id.btn_divide
+                        text = "/"
+                        onClick {
+                            num1 = if (et1.text.isNotEmpty()) et1.text.toString().toInt() else 0
+                            num2 = if (et2.text.isNotEmpty()) et2.text.toString().toInt() else 0
+                            tvRes.text = if (num2 != 0) (num1 / num2).toString() else "Not possible"
+                        }
+                    }.lparams {
+                        width = dip(0)
+                        height = wrapContent
+                        weight = 1F
                     }
                 }
             }
